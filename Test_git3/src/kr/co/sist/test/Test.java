372bfe8dbb09b23be4cbe2edfb41594f533ab79a
user <user@DESKTOP-COJ9858>
2023-08-22 12:39:05 +0900

    그의 3번째 도전중 3번쨰

--- Test_git3/src/kr/co/sist/test/Test.java
+++ Test_git3/src/kr/co/sist/test/Test.java
@@ -4,7 +4,10 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("시이파알");
+		System.out.println("두개재");
+		System.out.println("rsrs");
+		System.out.println("sadfjk");
 	}
 
 }
